96fab5dfb48afe0d0ba0aa8e1fd8ef3f065c0427
Menny Even Danan <menny@evendanan.net>
2016-01-19 17:59:35 -0500

    add content-provider which has applicationId rewrite placeholder

--- app/src/main/java/net/evendanan/bazelplayground/ScrollingActivity.java
+++ app/src/main/java/net/evendanan/bazelplayground/ScrollingActivity.java
@@ -1,13 +1,22 @@
 package net.evendanan.bazelplayground;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.ComponentInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ProviderInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import net.evendanan.rewrite_lib.MyContentProvider;
+
+import java.util.List;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -22,10 +31,26 @@ public class ScrollingActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Name of java lib is "+net.evendanan.bazelplayground.javalib.Util.getLibName(), Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Name of java lib is " + net.evendanan.bazelplayground.javalib.Util.getLibName(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+        ApplicationInfo appInfo = null;
+        try {
+            appInfo = getPackageManager().getApplicationInfo("net.evendanan.bazelplayground", PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        List<ProviderInfo> providers = getPackageManager().queryContentProviders(appInfo.processName, appInfo.uid, 0);
+        StringBuilder providersText = new StringBuilder("There are ");
+        providersText.append(providers.size()).append(" providers.");
+        for(ProviderInfo aProvider : providers) {
+            providersText.append("\nname:").append(aProvider.name);
+            providersText.append("\nauthority:").append(aProvider.authority);
+        }
+        TextView contentProviderText = (TextView) findViewById(R.id.context_provider_lib);
+        contentProviderText.setText(providersText.toString());
     }
 
     @Override
